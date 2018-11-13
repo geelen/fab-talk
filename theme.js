1d4d5022c2ff33db563a915695c554b2d599c77f
Glen Maddern <glenmaddern@gmail.com>
2018-11-13 11:48:38 +1100

    wip

--- theme.js
+++ theme.js
@@ -1,12 +1,19 @@
 import theme from 'mdx-deck/themes'
 
 export default {
+  // extends the default theme
   ...theme,
-
+  // add a custom font
+  font: 'Segoe UI, sans-serif',
+  // custom colors
+  colors: {
+    text: 'papayawhip',
+    background: 'black',
+    link: '#0ff',
+  }
+}
   // Customize your presentation theme here.
   //
   // Read the docs for more info:
   // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
   // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
-
-}
